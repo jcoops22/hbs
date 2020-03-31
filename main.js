6486a5e8b6b6303472cef7756721fd295716e00b
jcoops22 <jcooper232074@gmail.com>
2020-03-30 19:47:20 -0600

    Impimenting loading unseen images into the carosel

--- main.js
+++ main.js
@@ -73,13 +73,22 @@ about.addEventListener("mouseout", () => {
 // carosel
 let rotateLeft = new TimelineMax({ paused: true });
 
-let img1 = document.querySelector(".img1");
-let img2 = document.querySelector(".img2");
-let img3 = document.querySelector(".img3");
+// let img1 = document.querySelector(".img1");
+// let img2 = document.querySelector(".img2");
+// let img3 = document.querySelector(".img3");
+let img1 = document.querySelector(" .div1 img");
+let img2 = document.querySelector(".div2 img");
+let img3 = document.querySelector(".div3 img");
 let div1 = document.querySelector(".div1");
 let div2 = document.querySelector(".div2");
 let div3 = document.querySelector(".div3");
 
+const mensCuts = [
+  "./resources/mensCut.png",
+  "./resources/mensCut1.png",
+  "./resources/mensCut2.png",
+  "./resources/mensCut3.png"
+];
 const rotateClasses = ["left", "middle", "right"];
 let count1 = 0;
 let count2 = 1;
@@ -112,11 +121,30 @@ div1.addEventListener("click", () => {
     img3.classList.remove("right3");
   }
 });
-img1.addEventListener("mouseout", () => {
-  // count1 >= mensCuts.length - 1 ? (count1 = 0) : count1++;
-  // img1.src = mensCuts[count1];
-  // count2 >= mensCuts.length - 1 ? (count2 = 0) : count2++;
-  // img2.src = mensCuts[count2];
-  // count3 >= mensCuts.length - 1 ? (count3 = 0) : count3++;
-  // img3.src = mensCuts[count3];
+div3.addEventListener("click", () => {
+  if (img1.classList.contains("middle1")) {
+    console.log("img1 has middle");
+    img1.classList.remove("middle1");
+    img1.classList.add("right1");
+    img2.classList.remove("right2");
+    img2.classList.add("left2");
+    img3.classList.remove("left3");
+    img3.classList.add("middle3");
+  } else if (img1.classList.contains("right1")) {
+    console.log("img1 has right");
+    img1.classList.add("left1");
+    img1.classList.remove("right1");
+    img2.classList.add("middle2");
+    img2.classList.remove("left2");
+    img3.classList.add("right3");
+    img3.classList.remove("middle3");
+  } else {
+    console.log("img1 has nothing or left");
+    img1.classList.add("middle1");
+    img1.classList.remove("left1");
+    img2.classList.add("right2");
+    img2.classList.remove("middle2");
+    img3.classList.add("left3");
+    img3.classList.remove("right3");
+  }
 });
